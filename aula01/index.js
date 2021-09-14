798808f22d78677ec1116f457fe38ef299c1de36
Agatha França <agathafranca@outlook.com>
2021-09-14 10:43:51 -0300

    adicionando o código da calculadora de média

---
+++ aula01/index.js
@@ -0,0 +1,12 @@
+var nome = "Guilherme"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimentre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimentre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
