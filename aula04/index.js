21fbad7fbc71d1cfa0874e08c8d433cc4f5ee260
Agatha França <agathafranca@outlook.com>
2021-09-16 09:09:03 -0300

    imprimindo os nomes dos filmes na página através do método write da propriedade document

--- aula04/index.js
+++ aula04/index.js
@@ -1,3 +1,7 @@
 var filmeUm = "Yesterday"
 var filmeDois = "A Chegada"
 var filmeTres = "Escola de Rock"
+
+document.write("<p>" + filmeUm + "</p>")
+document.write("<p>" + filmeDois + "</p>")
+document.write("<p>" + filmeTres + "</p>")
