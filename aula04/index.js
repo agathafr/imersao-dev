42ed4f0d080f8916d15073c1ed6ea94b20f36e69
Agatha França <agathafranca@outlook.com>
2021-09-16 10:09:26 -0300

    trocando o nome da variável de iteração

--- aula04/index.js
+++ aula04/index.js
@@ -1,5 +1,5 @@
 var listaFilmes = ["https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg", "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg", "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/90/98/20169244.jpg"]
 
-for (var indice = 0; i < listaFilmes.length; i++){
+for (var i = 0; i < listaFilmes.length; i++){
     document.write("<p>" + listaFilmes[i] + "</p>")
 }
