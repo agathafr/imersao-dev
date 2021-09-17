7cb6b5f7cfcdabaf141f5963e78096933858412e
Agatha França <agathafranca@outlook.com>
2021-09-17 10:21:22 -0300

    refatorando o código

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function adicionarFilme() {
     var filmeFavorito = document.getElementById("filme").value
     if (filmeFavorito.endsWith(".jpg")) {
-        var elementoFilmeFavorito = "img src=" + campoFilmeFavorito + ">"
-        var elementoListaFilmes = document.getElementById("listaFilmes")
-        elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito
+        listarFilmesNaTela(filmeFavorito)
     } else {
         console.error("Endereço de filme inválido!")
     }
     document.getElementById("filme").value = ""
+}
+
+function listarFilmesNaTela(filmeFavorito) {
+    var elementoFilmeFavorito = "img src=" + filmeFavorito + ">"
+    var elementoListaFilmes = document.getElementById("listaFilmes")
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito
 }
